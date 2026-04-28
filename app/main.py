89d2072791ce1f969805689ed434c89fd37aa39e
李朝良 <lichaoliang@yanxiu.com>
2026-04-28 12:09:10 +0800

    提交一下

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from services.rabbitmq_service import RabbitMQService
+
 from app.api.v1.router import api_router
 
 from settings_config import settings
