4a3215d0768eeed45e324c49d4ebc37aed7b61af
李朝良 <lichaoliang@yanxiu.com>
2026-05-19 21:31:53 +0800

    提交一下

--- settings_config.py
+++ settings_config.py
@@ -10,7 +10,8 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
-        enable_decoding=False
+        enable_decoding=False,
+        env_ignore_empty=True
     )
 
     """全局配置"""
